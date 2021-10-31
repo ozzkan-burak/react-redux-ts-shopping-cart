ad975ded9b8d8645e231ef42aa26399fc3622fe0
Burak Özkan <burak.ozkan@path.com.tr>
2021-10-31 12:41:14 +0300

    implemet redx-toolkit

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import './App.css';
 import ProductForm from './components/Products/ProductForm';
 
 import ProductList from './components/Products/ProductList';
+import store from './redux/store/store';
 
 function App() {
   return (
+  <Provider store={store}>
     <div className="App">
       <ProductList />
       <ProductForm />
     </div>
+  </Provider>
   );
 }
 
--- src/components/Products/ProductList.tsx
+++ src/components/Products/ProductList.tsx
@@ -1,40 +1,23 @@
-import React, { useState } from "react";
+import React from "react";
+import { useSelector } from "react-redux";
+import { getProductsSelector } from "../../redux/Products/productslices";
 
-interface ProductListProps {
-
-}
-
-interface Product {
-  title: string;
-  price: number;
-  id: string;
-}
-
-const initialProducts = [
-  {title: "Escape From Tarkov", price: 60, id: "eft"},
-  {title: "Hunt: Showdown", price: 70, id: "hunt"},
-  {title: "Hell Let Loose", price: 55, id: "hll"},
-]
+interface ProductListProps {}
 
 const ProductList: React.FC<ProductListProps> = ({}) => {
 
-  const [products, setProducts] = useState<Product[]>(initialProducts);
+  const products  = useSelector(getProductsSelector);
 
   return (
     <div>
       <h2>Product List</h2>
 
-        {products.map(product => {
+        {products.map((product) => {
          return <div key={product.id}>
             <div>{`${product.title}: ${product.price}`}</div>
           </div>
         })}
-      <button onClick={()=> setProducts(prevProducts => [
-        {
-          title: "Half Life",
-          price: 100,
-          id:"hl" 
-        }, ...prevProducts])}>
+      <button>
         Add Product
       </button>
     </div>
