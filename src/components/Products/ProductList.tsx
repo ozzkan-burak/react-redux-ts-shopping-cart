7986ee1eec9fe5cbcd39e3708a86e74d9b36d227
Burak Özkan <burak.ozkan@path.com.tr>
2021-10-31 11:50:21 +0300

    add product list and product form component

--- src/components/Products/ProductList.tsx
+++ src/components/Products/ProductList.tsx
@@ -1,11 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 
 interface ProductListProps {
 
 }
 
+interface Product {
+  title: string;
+  price: number;
+  id: string;
+}
+
+const initialProducts = [
+  {title: "Escape From Tarkov", price: 60, id: "eft"},
+  {title: "Hunt: Showdown", price: 70, id: "hunt"},
+  {title: "Hell Let Loose", price: 55, id: "hll"},
+]
+
 const ProductList: React.FC<ProductListProps> = ({}) => {
-  return <div>ProductList</div>
+
+  const [products, setProducts] = useState<Product[]>(initialProducts);
+
+  return (
+    <div>
+      <h2>Product List</h2>
+
+        {products.map(product => {
+         return <div key={product.id}>
+            <div>{`${product.title}: ${product.price}`}</div>
+          </div>
+        })}
+      <button onClick={()=> setProducts(prevProducts => [
+        {
+          title: "Half Life",
+          price: 100,
+          id:"hl" 
+        }, ...prevProducts])}>
+        Add Product
+      </button>
+    </div>
+  )
 }
 
 export default ProductList
